1fae85d1b86187179b0468019713ea20ab4847fb
Solomon Achilles <solohag92@gmail.com>
2022-03-11 18:41:30 +0000

    Fix feature performers section difference in mobile view

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+const footer = document.querySelector('footer');
 /* Mobile Menu */
 
 const menuIcon = document.querySelector('.menu-icon');
@@ -20,6 +21,8 @@ a.style.textDecoration = 'none';
 a.style.color = 'white';
 li.appendChild(a);
 
+let aboutItem = ul.firstElementChild;
+
 const viewMobileMenu = () => {
   nav.classList.remove('menu-items');
   nav.className = 'modal';
@@ -27,6 +30,15 @@ const viewMobileMenu = () => {
   ul.insertBefore(li, firstListItem);
   ul.className = 'modal-list';
   document.body.style.overflow = 'hidden';
+  if (footer.className === 'footer'){
+    aboutItem.style.color = '#ec5242';
+    console.log('footer')
+  } else if (footer.className === 'about-footer'){
+    aboutItem.className = '';
+    aboutItem.style.color = '#fff';
+    a.style.color = '#ec5242';
+    console.log('about-footer')
+  }
 };
 
 const closeMobileMenu = () => {
@@ -38,9 +50,26 @@ const closeMobileMenu = () => {
   document.body.style.overflow = 'auto';
 };
 
+const closeMobileMenuOnDsktopView = (e) => {
+  const width = e.target.outerWidth;
+  if (width > '767') {
+    nav.classList.remove('modal');
+    nav.className = 'menu-items';
+    ul.classList.remove('modal-list');
+    ul.appendChild(lastListItem);
+    document.body.style.overflow = 'auto';
+  }
+};
+
+
+
 menuIcon.addEventListener('click', viewMobileMenu);
 
 closeButton.addEventListener('click', closeMobileMenu);
+
+window.addEventListener('resize', (e) => {
+  closeMobileMenuOnDsktopView(e);
+});
 
 /* Feature Singers */
 
@@ -49,35 +78,36 @@ const singers = [
     name: 'Pax Choir Knust',
     image: './public/assets/images/pax-choir.png',
     occupation: 'Activity: Singing',
-    details: 'Melodious hymnals & Choral music',
+    details: 'They sing melodious hymnals & Choral music',
   },
   {
     name: 'Lawyer Nti',
-    image: './public/assets/images/drfilfree1.png',
+    image: './public/assets/images/lawyer-nti.jpeg',
     occupation: 'Activity: Comedy',
-    details: 'Mr. Richmond Xavier Amoakoh who starred in the popular TV show Kejetia' 
-             + 'VS Makola was a lawyer for Kegetia law school will grace the occation' 
+    details: 'Mr. Richmond Xavier Amoakoh who starred in the popular TV show Kejetia ' 
+             + 'VS Makola was a lawyer for Kegetia law school will grace the occation ' 
              + 'with some wonderful performances.',
   },
   {
     name: 'Aba.the.poet',
     image: './public/assets/images/aba.the.poet.jpg',
     occupation: 'Activity: Poetry and Spoken word',
-    details: "Miss Francisca Aba Baffoe as she's formally called has always trilled" 
+    details: "Miss Francisca Aba Baffoe as she's formally called has always trilled " 
             + "her audience with great inspiring poems.",
   },
   {
     name: 'GHAMSU Choir',
     image: './public/assets/images/ghamsu-choir.jpg',
     occupation: 'Activity: Singing',
-    details: 'Melodious hymnals & Choral music',
+    details: 'They sing melodious hymnals & Choral music',
   },
 
   {
     name: 'Heralds of Honour',
     image: './public/assets/images/heralds-of-honour.jpeg',
     occupation: 'Activity: Drama & Choreography',
-    details: '',
+    details: 'They are well known for their various creative plays when preaches the ' 
+           + 'word of God to the audience.',
   },
 ];
 
@@ -87,38 +117,101 @@ const title = document.createElement('h2');
 title.className = 'feature-title';
 title.textContent = 'Featured Performers';
 const span = document.createElement('span');
-const cardDiv = document.createElement('div');
+let cardDiv = document.createElement('div');
 cardDiv.className = 'singers-container';
 span.className = 'border-span';
 section.append(title, span);
 
-const footer = document.querySelector('.footer');
+const screenWidth = window.matchMedia('(min-width: 768px)');
 const main = document.querySelector('main');
 
-singers.forEach((singer) => {
-  const outerDiv = document.createElement('div');
-  outerDiv.className = 'singer-container';
-  const innerDiv = document.createElement('div');
-  innerDiv.className = 'singer-details';
-  const imgDiv = document.createElement('div');
-  imgDiv.className = 'performer-image-container';
-  const image = document.createElement('img');
-  image.className = 'performer-image';
-  const name = document.createElement('h2');
-  const details = document.createElement('p');
+const moreButton = document.createElement('button');
+moreButton.className = 'more-button';
+moreButton.type = 'button';
+moreButton.textContent = 'MORE';
+const downArrowIcon = document.createElement('i');
+downArrowIcon.className = 'fas fa-arrow-down';
+moreButton.appendChild(downArrowIcon);
 
-  details.textContent = singer.details;
-  name.textContent = singer.name;
-  image.src = singer.image;
-  image.alt = singer.name;
-  innerDiv.append(name, details);
-  imgDiv.appendChild(image);
-  outerDiv.append(imgDiv, innerDiv);
-  cardDiv.appendChild(outerDiv);
-});
+const addSinger = () => {
+  singers.forEach((singer) => {
+    const outerDiv = document.createElement('div');
+    outerDiv.className = 'singer-container';
+    const innerDiv = document.createElement('div');
+    innerDiv.className = 'singer-details';
+    const imgDiv = document.createElement('div');
+    imgDiv.className = 'performer-image-container';
+    const image = document.createElement('img');
+    image.className = 'performer-image';
+    const name = document.createElement('h2');
+    const details = document.createElement('p');
+    const occupation = document.createElement('span');
+    occupation.className = 'performer-activity';
+    const borderBottom = document.createElement('span');
+    borderBottom.className = 'performer-bottom-border';
+    occupation.textContent = singer.occupation;
+    details.textContent = singer.details;
+    name.textContent = singer.name;
+    image.src = singer.image;
+    image.alt = singer.name;
+    innerDiv.append(name, occupation, borderBottom, details);
+    imgDiv.appendChild(image);
+    outerDiv.append(imgDiv, innerDiv);
+    if (screenWidth.matches) {
+    cardDiv.appendChild(outerDiv);
+    } else {
+      if (singers.indexOf(singer) < 2) {
+        cardDiv.appendChild(outerDiv);
+      }
+    }  
+  });
+}
+
+addSinger();
+
+window.addEventListener('resize', () => {
+    if (cardDiv.childElementCount === 0){
+      addSinger();
+    } else if (cardDiv.childElementCount <= 2){
+      while(cardDiv.firstChild){
+        cardDiv.firstChild.remove();
+      }
+      addSinger();
+    } else if (cardDiv.childElementCount <= 5 && !screenWidth.matches) {
+      while(cardDiv.firstChild){
+        cardDiv.firstChild.remove();
+      }
+      addSinger();
+    }
+  });
+
+
+// const checkScreenSize = () => {
+//   if (screenWidth.matches){
+//     section.appendChild(cardDiv);
+//     console.log(section);
+//   } else {
+//     const moreButton = document.createElement('button');
+//       moreButton.textContent = `More <i class="fas fa-arrow-down"></i>`;
+//       let children = cardDiv.childNodes;  
+//       for (let i = 0; i < children.length; i++) {
+//         if (i < 2) {
+//           section.appendChild(children[i]);
+//           console.log(section);
+//         }
+//       }
+//   }
+// }
+
+// window.addEventListener('resize', () => {
+//  checkScreenSize();
+// });
+
+// checkScreenSize();
 
 section.appendChild(cardDiv);
+section.appendChild(moreButton);
 
-if (footer !== null) {
+if (footer.className === 'footer') {
   main.insertBefore(section, footer);
 }
